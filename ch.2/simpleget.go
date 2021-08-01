755c49a399dceb54a951f58a79d067ea536eeff8
waneal <37803125+waneal@users.noreply.github.com>
2021-08-01 09:52:19 +0900

    add go files

--- ch.2/simpleget.go
+++ ch.2/simpleget.go
@@ -1,1 +1,51 @@
-package ch_2
+package main
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"net/textproto"
+	"os"
+)
+
+func main() {
+	// values := url.Values{
+	// 	"query": {"hello world"},
+	// }
+
+	var buffer bytes.Buffer
+	writer := multipart.NewWriter(&buffer)
+	writer.WriteField("name", "Hitoshi Fujita")
+
+	part := make(textproto.MIMEHeader)
+	part.Set("Context-Type", "image/jpeg")
+	part.Set("Content-Disposition", `form-data; name="thumbnail"; filename="thumbnail.jpg"`)
+	fileWriter, err := writer.CreatePart(part)
+	// fileWriter, err := writer.CreateFormFile("thumbnail","thumbnail.jpg")
+	if err != nil {
+		panic(err)
+	}
+	readFile, err := os.Open("thumbnail.jpg")
+	if err != nil {
+		panic(err)
+	}
+	defer readFile.Close()
+	io.Copy(fileWriter, readFile)
+	writer.Close()
+
+	resp, err := http.Post("http://localhost:18888", writer.FormDataContentType(), &buffer)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(body))
+	log.Println("Status:", resp.Status)
+	log.Println("Headers:", resp.Header)
+}
